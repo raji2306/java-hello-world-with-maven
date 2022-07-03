2ab3d870a687ebcd7c5afebf52d2518c17e4419d
raji2306 <94977452+raji2306@users.noreply.github.com>
2022-07-04 04:19:33 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        return "Mr. Rajesh Suresh Senior Cloud DevOps Engineer";
     }
 }
