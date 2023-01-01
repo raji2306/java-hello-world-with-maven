5df7bd8d2b77f51062fb27b050b8b11b4b5f6e53
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-01-01 19:38:21 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,6 +1,8 @@
 package hello;
 
 import org.joda.time.LocalTime;
+import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+import com.rollbar.notifier.Rollbar;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -9,5 +11,10 @@ public class HelloWorld {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+	    
+    Rollbar rollbar = Rollbar.init(withAccessToken("f14a1ad6e0e644009d456897cfab76ac").build());
+    rollbar.log("Hello, Rollbar");
+    rollbar.close(true);
+	    
     }
 }
